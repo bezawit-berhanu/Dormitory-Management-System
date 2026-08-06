7a507d42a99af306fafe7a19f4f2b1e4c141de1b
EhitneshWale <ehitneshwale@gmail.com>
2026-08-06 13:11:24 +0300

    Refactor ComplaintResponse class structure
    Removed duplicate class definition and merged properties.

--- src/DormitoryManagementSystem.Domain/Entities/ComplaintResponse.cs
+++ src/DormitoryManagementSystem.Domain/Entities/ComplaintResponse.cs
@@ -1,6 +1,4 @@
 namespace DormitoryManagementSystem.Domain.Entities;
-<<<<<<< HEAD
-
 public class ComplaintResponse
 {
     public int ResponseId { get; set; }
@@ -18,13 +16,4 @@ public class ComplaintResponse
     public string Response { get; set; } = string.Empty;
 
     public DateTime ResponseDate { get; set; }
-}
-=======
-public class ComplaintResponse
-{
-    public int Id { get; set; }
-    public int ComplaintId { get; set; }
-    public string ResponseText { get; set; } = string.Empty;
-    public DateTime DateResponded { get; set; }
 };
->>>>>>> ca6ddbeeb2d8d4bf0827dae4d9461809e589ba28
