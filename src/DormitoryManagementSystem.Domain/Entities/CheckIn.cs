e2d75ea19c8b08d8c096b6be247d98b4873dd2e1
EhitneshWale <ehitneshwale@gmail.com>
2026-08-06 13:08:35 +0300

    Refactor CheckIn class to remove duplicates
    Removed duplicate CheckIn class definition and cleaned up the code.

--- src/DormitoryManagementSystem.Domain/Entities/CheckIn.cs
+++ src/DormitoryManagementSystem.Domain/Entities/CheckIn.cs
@@ -1,5 +1,4 @@
 namespace DormitoryManagementSystem.Domain.Entities;
-<<<<<<< HEAD
 
 public class CheckIn
 {
@@ -16,12 +15,3 @@ public class CheckIn
     public int CheckedInByUserId { get; set; }
     public User? CheckedInByUser { get; set; }
 }
-=======
-public class CheckIn
-{
-    public int Id { get; set; }
-    public int StudentId { get; set; }
-    public int RoomId { get; set; }
-    public DateTime CheckInDate { get; set; }
-};
->>>>>>> ca6ddbeeb2d8d4bf0827dae4d9461809e589ba28
