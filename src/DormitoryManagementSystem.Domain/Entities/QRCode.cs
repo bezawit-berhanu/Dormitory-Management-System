583def02585b13dff2cb8d6dcd8832b04319887c
EhitneshWale <ehitneshwale@gmail.com>
2026-08-06 14:01:50 +0300

    Refactor QRCode class to eliminate duplicates
    Removed duplicate QRCode class definition and merged properties.

--- src/DormitoryManagementSystem.Domain/Entities/QRCode.cs
+++ src/DormitoryManagementSystem.Domain/Entities/QRCode.cs
@@ -1,6 +1,4 @@
 namespace DormitoryManagementSystem.Domain.Entities;
-<<<<<<< HEAD
-
 public class QRCode
 {
     public int QRCodeId { get; set; }
@@ -16,11 +14,5 @@ public class QRCode
     public DateTime ExpiryDate { get; set; }
 
     public string Status { get; set; } = string.Empty;
-}
-=======
-public class QRCode
-{
-    public int Id { get; set; }
-    public string Code { get; set; } = string.Empty;
 };
->>>>>>> ca6ddbeeb2d8d4bf0827dae4d9461809e589ba28
+
