8cc11cba6ea597a01624c4f7e5a2248d5296e2b0
EhitneshWale <ehitneshwale@gmail.com>
2026-08-06 14:01:08 +0300

    Update InspectionFinding.cs

--- src/DormitoryManagementSystem.Domain/Entities/InspectionFinding.cs
+++ src/DormitoryManagementSystem.Domain/Entities/InspectionFinding.cs
@@ -1,5 +1,4 @@
 namespace DormitoryManagementSystem.Domain.Entities;
-<<<<<<< HEAD
 
 public class InspectionFinding
 {
@@ -18,12 +17,5 @@ public class InspectionFinding
     public string Recommendation { get; set; } = string.Empty;
 
     public string Status { get; set; } = string.Empty;
-}
-=======
-public class InspectionFinding
-{
-    public int Id { get; set; }
-    public int InspectionId { get; set; }
-    public string FindingDescription { get; set; } = string.Empty;
 };
->>>>>>> ca6ddbeeb2d8d4bf0827dae4d9461809e589ba28
+
