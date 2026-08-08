b478a6f1db0b6194adec2a8ba9896d2e969a9103
EhitneshWale <ehitneshwale@gmail.com>
2026-08-08 13:20:07 +0300

    Rename Id to SId in CheckOut entity

--- src/DormitoryManagementSystem.Domain/Entities/CheckOut.cs
+++ src/DormitoryManagementSystem.Domain/Entities/CheckOut.cs
@@ -5,7 +5,7 @@ public class CheckOut
     public int CheckOutId { get; set; }
 
     // Foreign Key: Student
-    public int Id { get; set; }
+    public int SId { get; set; }
     public Student? Student { get; set; }
 
     // Foreign Key: RoomAssignment
